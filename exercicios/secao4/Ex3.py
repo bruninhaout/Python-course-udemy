afbecfebcdea8d578ff60de4fb6e4cc70b03fe31
Bruna Cordeiro <65495514+bruninhaout@users.noreply.github.com>
2021-09-10 18:27:10 -0300

    Create Ex3.py

---
+++ exercicios/secao4/Ex3.py
@@ -0,0 +1,7 @@
+# digitar tres valores inteiros e imprimir a soma
+print("Digite três números inteiros:\n")
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+soma = num1 + num2 + num3
+print(f'A soma é: {soma}')
