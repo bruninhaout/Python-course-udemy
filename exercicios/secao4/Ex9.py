eef8943e6ed69e5f57aa781dfd294d721dde4109
Bruna Cordeiro <65495514+bruninhaout@users.noreply.github.com>
2021-09-11 11:05:50 -0300

    Create Ex9.py

---
+++ exercicios/secao4/Ex9.py
@@ -0,0 +1,5 @@
+# ler em graus Celsius, converter para graus Kelvin
+
+celsius = float(input('Insira a temperatura em graus Celsius = '))
+kelvin = celsius + 273.15
+print(f'A temperatura {celsius}°C em graus Celsius é {kelvin}°K')
