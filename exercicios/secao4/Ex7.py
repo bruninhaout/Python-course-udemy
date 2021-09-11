e871aab39c9cd0e8416670e1fffb520125d4a485
Bruna Cordeiro <65495514+bruninhaout@users.noreply.github.com>
2021-09-11 11:05:00 -0300

    Create Ex7.py

---
+++ exercicios/secao4/Ex7.py
@@ -0,0 +1,5 @@
+# ler em graus Fahrenheit, converter para graus Celsius
+
+fahr = float(input('Insira a temperatura em graus Fahrenheit = '))
+celsius = 5 * (fahr -  32) / 9
+print(f'A temperatura {fahr}°F em Fahrenheit é {celsius}°C')
