60e71b834ed9a47847c15b6534d1cced90d44750
Bruna Cordeiro <65495514+bruninhaout@users.noreply.github.com>
2021-09-13 19:20:13 -0300

    Create Ex29.py

---
+++ exercicios/secao4/Ex29.py
@@ -0,0 +1,9 @@
+# ler quatros notas e calcular a média aritmética
+
+nota1 = float(input('Nota 1 = '))
+nota2 = float(input('Nota 2 = '))
+nota3 = float(input('Nota 3 = '))
+nota4 = float(input('Nota 4 = '))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+print(f'Media = {media}')
