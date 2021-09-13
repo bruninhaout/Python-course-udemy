d08af9640871620e6b4847771255dc15e215903e
Bruna Cordeiro <65495514+bruninhaout@users.noreply.github.com>
2021-09-13 19:19:59 -0300

    Create Ex28.py

---
+++ exercicios/secao4/Ex28.py
@@ -0,0 +1,8 @@
+# ler tres valores e exibir a soma dos quadrados dos tres valores
+
+num1 = float(input('Primeiro número = '))
+num2 = float(input('Segundo número = '))
+num3 = float(input('Terceiro número = '))
+
+result = num1**2 + num2**2 + num3**2
+print(result)
