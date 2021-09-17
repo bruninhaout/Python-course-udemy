11b754786db30e4400d7d2ce225627e7c4f89d3f
Bruna Cordeiro <65495514+bruninhaout@users.noreply.github.com>
2021-09-16 21:36:33 -0300

    Create Ex39.py

---
+++ exercicios/secao4/Ex39.py
@@ -0,0 +1,16 @@
+"""
+Do valor total de R$ 780.000,00 dividir entre três ganhadores de um concurso
+• Primeiro -> 46%
+• Segundo -> 32%
+• Terceiro -> restante (22%)
+"""
+
+total =float(780000)
+pri = total * 0.46
+seg = total * 0.32
+ter = total * 0.22
+
+print(f'O Primeiro ganhará -> R$ {pri}')
+print(f'O Segundo ganhará -> R$ {seg}')
+print(f'E o Terceiro ganhará -> R$ {ter}')
+
