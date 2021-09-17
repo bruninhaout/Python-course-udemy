d7cb45f10637aeb25653f23e778c079b7f54db22
Bruna Cordeiro <65495514+bruninhaout@users.noreply.github.com>
2021-09-16 21:35:47 -0300

    Create Ex36.py

---
+++ exercicios/secao4/Ex36.py
@@ -0,0 +1,7 @@
+# altura e raio de um cilindro e imprimir volume
+
+pi = 3.141592
+h = float(input('Altura do cilindro = '))
+r = float(input('Raio do cilindro = '))
+vol = pi * (r**2) * h
+print(f'Volume do cilindro = {vol}')
